d0ef0039f7288ef24925184132fd44e57c943159
FriquetLuca <friquet.luca@gmail.com>
2022-08-03 12:09:26 +0200

    Calculator parse

--- main.js
+++ main.js
@@ -261,10 +261,10 @@ const controlsBasicPattern = new BasicPattern({
     }
 });
 const blockNestedPatterns = new BasicPattern({
-    name: 'Curly Brackets',
-    defaultValue: '{}',
-    isPattern: (i, c, txt) => { return c === '{'; },
-    isPatternEnd: (i, c, txt) => { return c === '}'; },
+    name: 'Brackets',
+    defaultValue: '()',
+    isPattern: (i, c, txt) => { return c === '('; },
+    isPatternEnd: (i, c, txt) => { return c === ')'; },
     fetch: (index, c, txt, endPattern, patternSet) => {
         let p = LookForPattern(txt, patternSet, index + 1, endPattern); // Let's look for nested pattern over here..
         // We could filter patternSet if we wanted to get rid of some functions for this case or use whatever we want anyway.
@@ -356,7 +356,507 @@ const separateNodes = (nodes, depth = 0) => {
     }
     return toShow;
 };
-
+const prioOpe = {
+    '+': 1,
+    '-': 1,
+    '*': 0,
+    '/': 0,
+    '%': 0
+}
+const preFunc = {
+    'cos': {
+        nbrArg: 1,
+        exec: (e) => {
+            return Math.cos(e);
+        }
+    },
+    'sin': {
+        nbrArg: 1,
+        exec: (e) => {
+            return Math.sin(e);
+        }
+    },
+    'tan': {
+        nbrArg: 1,
+        exec: (e) => {
+            return Math.tan(e);
+        }
+    },
+    'cosh': {
+        nbrArg: 1,
+        exec: (e) => {
+            return Math.cosh(e);
+        }
+    },
+    'sinh': {
+        nbrArg: 1,
+        exec: (e) => {
+            return Math.sinh(e);
+        }
+    },
+    'tanh': {
+        nbrArg: 1,
+        exec: (e) => {
+            return Math.tanh(e);
+        }
+    },
+    'acos': {
+        nbrArg: 1,
+        exec: (e) => {
+            return Math.acos(e);
+        }
+    },
+    'asin': {
+        nbrArg: 1,
+        exec: (e) => {
+            return Math.asin(e);
+        }
+    },
+    'atan': {
+        nbrArg: 1,
+        exec: (e) => {
+            return Math.atan(e);
+        }
+    },
+    'atann': {
+        nbrArg: 2,
+        exec: (y, x) => {
+            return Math.atan2(y, x);
+        }
+    },
+    'arcosh': {
+        nbrArg: 1,
+        exec: (e) => {
+            return Math.acosh(e);
+        }
+    },
+    'arsinh': {
+        nbrArg: 1,
+        exec: (e) => {
+            return Math.asinh(e);
+        }
+    },
+    'artanh': {
+        nbrArg: 1,
+        exec: (e) => {
+            return Math.atanh(e);
+        }
+    },
+    'min': {
+        nbrArg: 2,
+        unlockLimit: true,
+        exec: (...a) => {
+            return Math.min(...a);
+        }
+    },
+    'max': {
+        nbrArg: 2,
+        unlockLimit: true,
+        exec: (...a) => {
+            return Math.max(...a);
+        }
+    },
+    'abs': {
+        nbrArg: 1,
+        exec: (e) => {
+            return Math.abs(e);
+        }
+    },
+    'ceil': {
+        nbrArg: 1,
+        exec: (e) => {
+            return Math.ceil(e);
+        }
+    },
+    'floor': {
+        nbrArg: 1,
+        exec: (e) => {
+            return Math.floor(e);
+        }
+    },
+    'round': {
+        nbrArg: 1,
+        exec: (e) => {
+            return Math.round(e);
+        }
+    },
+    'square': {
+        nbrArg: 1,
+        exec: (e) => {
+            return e * e;
+        }
+    },
+    'sqrt': {
+        nbrArg: 1,
+        exec: (e) => {
+            return Math.sqrt(e);
+        }
+    },
+    'root': {
+        nbrArg: 2,
+        exec: (n, x) => {
+            return Math.pow(x, 1 / n);
+        }
+    },
+    'pow': {
+        nbrArg: 2,
+        exec: (x, n) => {
+            return Math.pow(x, n);
+        }
+    },
+    'exp': {
+        nbrArg: 1,
+        exec: (x) => {
+            return Math.exp(x, n);
+        }
+    },
+    'ln': {
+        nbrArg: 1,
+        exec: (x) => {
+            return Math.log(x);
+        }
+    },
+    'log': {
+        nbrArg: 2,
+        exec: (b, x) => {
+            return Math.log(x) / Math.log(b);
+        }
+    },
+    'factorial': {
+        nbrArg: 1,
+        exec: (x) => {
+            return gamma(x + 1);
+        }
+    },
+    'gamma': {
+        nbrArg: 1,
+        exec: (x) => {
+            return gamma(x);
+        }
+    },
+    'lambertW': {
+        nbrArg: 1,
+        exec: (x) => {
+            return lambertW(x);
+        }
+    },
+    'ilog': {
+        nbrArg: 2,
+        exec: (b, x) => {
+            return iLog(x, b);
+        }
+    },
+    'random': {
+        nbrArg: 2,
+        exec: (a, b) => {
+            return Math.random() * (b - a) + a;
+        }
+    }
+};
+function iLog(n, b)
+{
+    let x = 0.0;
+    while (n > 1.0)
+    {
+        x += 1.0;
+        n = Math.log(n) / Math.log(b);
+    }
+    return x;
+}
+function gamma(a)
+{
+    let s = 2.48574089138753565546e-5;
+    if (a < 0.5)
+    {
+        s += 1.05142378581721974210 / (1.0 - a);
+        s += -3.45687097222016235469 / (2.0 - a);
+        s += 4.51227709466894823700 / (3.0 - a);
+        s += -2.98285225323576655721 / (4.0 - a);
+        s += 1.05639711577126713077 / (5.0 - a);
+        s += -1.95428773191645869583e-1 / (6.0 - a);
+        s += 1.70970543404441224307e-2 / (7.0 - a);
+        s += -5.71926117404305781283e-4 / (8.0 - a);
+        s += 4.63399473359905636708e-6 / (9.0 - a);
+        s += -2.71994908488607703910e-9 / (10.0 - a);
+        return 3.14159265358979323846264338327950288419716939937510582 / (Math.sin(3.14159265358979323846264338327950288419716939937510582 * a) * s * 1.8603827342052657173362492472666631120594218414085755 * Math.pow((a - 10.400511) / 2.71828182845904523536028747135266249, 0.5 - a));
+    }
+    else
+    {
+        s += 1.05142378581721974210 / a;
+        s += -3.45687097222016235469 / (a + 1.0);
+        s += 4.51227709466894823700 / (a + 2.0);
+        s += -2.98285225323576655721 / (a + 3.0);
+        s += 1.05639711577126713077 / (a + 4.0);
+        s += -1.95428773191645869583e-1 / (a + 5.0);
+        s += 1.70970543404441224307e-2 / (a + 6.0);
+        s += -5.71926117404305781283e-4 / (a + 7.0);
+        s += 4.63399473359905636708e-6 / (a + 8.0);
+        s += -2.71994908488607703910e-9 / (a + 9.0);
+        return s * 1.8603827342052657173362492472666631120594218414085755 * Math.pow((a + 10.400511) / 2.71828182845904523536028747135266249, a - 0.5);
+    }
+}
+function lambertW(x)
+{
+    if (x < -Math.exp(-1))
+    {
+        throw new Error("The Lambert W function is not defined for " + x + ".");
+    }
+    let amountOfIterations = Math.max(4, Math.ceil(Math.log(x) / 3));
+    let w = 3 * Math.log(x + 1) / 4;
+    for (let i = 0; i < amountOfIterations; i++)
+    {
+        w -= (w * Math.exp(w) - x) / (Math.exp(w) * (w + 1) - (w + 2) * (w * Math.exp(w) - x) / (2 * w + 2));
+    }
+    return w;
+}
+const remakeNodes = (nodes) => {
+    let result = [];
+    for(let i = 0; i < nodes.length; i++)
+    {
+        let node = nodes[i];
+        if(typeof node[1] === 'object') // This node is a sub element (an array if nothing goes wrong)
+        {
+            result.push({
+                name: node[0],
+                content: remakeNodes(node[1])
+            });
+        }
+        else // It's a string, ez pz let's write it with some spacing
+        {
+            switch(node[0])
+            {
+                case 'Whitespace':
+                    break;
+                case 'Word':
+                    if(preFunc[node[1]]) // Is a function
+                    {
+                        if(i + 1 < nodes.length)
+                        {
+                            let nextNode = nodes[i + 1];
+                            if(nextNode[0] === 'Brackets')
+                            {
+                                result.push({
+                                    name: 'Function',
+                                    funcName: node[1],
+                                    content: remakeNodes(nextNode[1])
+                                });
+                                i++;
+                            }
+                            else
+                            {
+                                /*
+                                result.push({ // Probably usefull
+                                    name: node[0],
+                                    content: node[1]
+                                });*/
+                            }
+                        }
+                        else
+                        {
+                            // Discard..
+                        }
+                    }
+                    else
+                    {
+                        switch(node[1])
+                        {
+                            case 'ϕ':
+                            case 'golden':
+                                result.push({
+                                    name: 'Number',
+                                    content: 1.618033988749
+                                });
+                                break;
+                            case 'eulermascheroni':
+                                result.push({
+                                    name: 'Number',
+                                    content: 0.57721566490153286060651209008240243104215933593992
+                                });
+                                break;
+                            case 'apery':
+                                result.push({
+                                    name: 'Number',
+                                    content: 1.202056903159594285399738161511449990764986292
+                                });
+                                break;
+                            case 'e':
+                                result.push({
+                                    name: 'Number',
+                                    content: 2.7182818284590452353602874713527
+                                });
+                                break;
+                            case 'π':
+                            case 'pi':
+                                result.push({
+                                    name: 'Number',
+                                    content: 3.1415926535897932384626433832795
+                                });
+                                break;
+                        }
+                        // Garbage, discarding in progress..
+                    }
+                    break;
+                case 'Symbol':
+                    if(node[1] === '°')
+                    {
+                        result.push({
+                            name: 'Symbol',
+                            content: '*'
+                        });
+                        result.push({
+                            name: 'Number',
+                            content: 0.01745329251994329576923690768489
+                        });
+                    }
+                    else
+                    {
+                        result.push({
+                            name: node[0],
+                            content: node[1]
+                        });
+                    }
+                    break;
+                default:
+                    result.push({
+                        name: node[0],
+                        content: node[1]
+                    });
+                    break;
+            }
+        }
+    }
+    return result;
+};
+const computeResult = (nodes, priority = 0) => {
+    if(priority > 2)
+    {
+        return nodes;
+    }
+    for(let i = 0; i < nodes.length; i++)
+    {
+        if(i < 0)
+        {
+            i = 0;
+        }
+        let node = nodes[i];
+        if(node.name === 'Function')
+        {
+            let f = preFunc[node.funcName];
+            let result = computeResult(node.content);
+            if(f.unlockLimit)
+            {
+                let prepareArgs = [];
+                for(let node of result)
+                {
+                    if(node.name === 'Punctuation')
+                    {
+                        continue;
+                    }
+                    prepareArgs.push(node.content);
+                }
+                nodes[i].name = 'Number';
+                nodes[i].content = f.exec(...prepareArgs);
+                i -= 2;
+            }
+            else if(result.length == 2 * f.nbrArg - 1) // Should be the same number of arguments
+            {
+                let prepareArgs = [];
+                for(let node of result)
+                {
+                    if(node.name === 'Punctuation')
+                    {
+                        continue;
+                    }
+                    prepareArgs.push(node.content);
+                }
+                nodes[i].name = 'Number';
+                nodes[i].content = f.exec(...prepareArgs);
+                i -= 2;
+            }
+        }
+        else if(node.name === 'Brackets')
+        {
+            let result = computeResult(node.content);
+            nodes[i] = result[0];
+            i -= 2;
+        }
+        if(i - 1 >= 0 && i + 1 < nodes.length) // Element between 2 other
+        {
+            let prevNode = nodes[i - 1];
+            let nextNode = nodes[i + 1];
+            if(node.name === 'Symbol')
+            if(prioOpe[node.content] == priority)
+            {
+                switch(node.content)
+                {
+                    case '+':
+                        if(prevNode.name === 'Number' && nextNode.name === 'Number')
+                        {
+                            nodes[i - 1].content = prevNode.content + nextNode.content;
+                            nodes.splice(i, 2);
+                            i--;
+                        }
+                        break;
+                    case '-':
+                        if(prevNode.name === 'Number' && nextNode.name === 'Number')
+                        {
+                            nodes[i - 1].content = prevNode.content - nextNode.content;
+                            nodes.splice(i, 2);
+                            i--;
+                        }
+                        break;
+                    case '*':
+                        if(prevNode.name === 'Number' && nextNode.name === 'Number')
+                        {
+                            nodes[i - 1].content = prevNode.content * nextNode.content;
+                            nodes.splice(i, 2);
+                            i--;
+                        }
+                        break;
+                    case '/':
+                        if(prevNode.name === 'Number' && nextNode.name === 'Number')
+                        {
+                            nodes[i - 1].content = prevNode.content / nextNode.content;
+                            nodes.splice(i, 2);
+                            i--;
+                        }
+                        break;
+                    case '%':
+                        if(prevNode.name === 'Number' && nextNode.name === 'Number')
+                        {
+                            nodes[i - 1].content = prevNode.content % nextNode.content;
+                            nodes.splice(i, 2);
+                            i--;
+                        }
+                        break;
+                }
+            }
+        }
+        else if(i == 0 && i + 1 < nodes.length) // First element
+        {
+            let nextNode = nodes[i + 1];
+            if(node.name === 'Symbol')
+            {
+                switch(node.content)
+                {
+                    case '-':
+                        if(nextNode.name === 'Number')
+                        {
+                            nodes[i + 1].content = -nodes[i + 1].content;
+                        }
+                        nodes.splice(0, 1);
+                        i--;
+                        break;
+                    case '+':
+                        nodes.splice(0, 1);
+                        i--;
+                        break;
+                }
+            }
+        }
+    }
+    return computeResult(nodes, priority + 1);
+}
 
 function fastInput()
 {
@@ -374,11 +874,11 @@ function fastInput()
         logs.pop();
     }
     userLog.innerHTML = `<pre>${packLogs}</pre>`;
-    output.innerHTML = `<pre>${separateNodes(subdivided.result)}</pre>`;
+    output.innerHTML = `<pre>${computeResult(remakeNodes(subdivided.result))[0].content}</pre>`;
 }
 fastInput();
 
 let userInput = document.getElementById('userInput');
-userInput.addEventListener('keyup', (e) => {
+userInput.addEventListener('input', (e) => {
     fastInput();
 })
